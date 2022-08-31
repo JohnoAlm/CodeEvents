2bc4d9c2445b095c1b40a9485df847e7361a7b69
almenoliver02@gmail.com <almenoliver02@gmail.com>
2022-08-31 15:02:05 +0200

    Added Dto and lecture repository

---
+++ CodeEvents.Api/Core/Dto/LectureDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeEvents.Api.Core.Dto
+{
+
+#nullable disable
+
+    public class LectureDto
+    {
+        public string Title { get; set; }
+        public int Level { get; set; }
+
+        public int SpeakerId { get; set; }
+    }
+}
